16b98ac36988c0ec746c290aba4d29547797f038
Ricky Rozian <rrozian00@gmail.com>
2025-05-22 08:17:13 +0700

    asjkdgh

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rhome/cores/app/my_app.dart';
 import 'package:rhome/cores/helpers/app_bloc_observer.dart';
+import 'package:rhome/firebase_options.dart';
 
-void main() {
+void main() async {
   Bloc.observer = AppBlocObserver();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
