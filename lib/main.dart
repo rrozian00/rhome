b895edce2d9435bb512e0511e0d16af2aebb187e
rrozian00 <rrozian00@gmail.com>
2025-06-30 21:57:16 +0700

    oke3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rhome/features/auth/login/bloc/login_bloc.dart';
-import 'package:rhome/features/home/presentation/views/home_view.dart';
 
 import 'cores/app/app_injection.dart';
 import 'cores/app/app_providers.dart';
@@ -10,7 +8,7 @@ import 'cores/app/routes.dart';
 import 'cores/helpers/app_bloc_observer.dart';
 import 'cores/preferences/colors.dart';
 import 'cores/preferences/themes/light_theme.dart';
-import 'features/auth/login/page/login_view.dart';
+import 'features/auth/login/bloc/login_bloc.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -32,18 +30,11 @@ class MyApp extends StatelessWidget {
         builder: (context) {
           context.read<LoginBloc>().add(CheckAuth());
 
-          return BlocListener<LoginBloc, LoginState>(
-            listener: (context, state) {
-              // if (state is LoginInitial) {
-              //   context.read<LoginBloc>().add(CheckAuth());
-              // }
-            },
-            child: MaterialApp(
-              onGenerateRoute: routes,
-              // initialRoute: LoginView.routeName,
-              title: 'RHome',
-              theme: LightTheme(AppColors.black).theme,
-            ),
+          return MaterialApp(
+            onGenerateRoute: routes,
+            // initialRoute: LoginView.routeName,
+            title: 'RHome',
+            theme: LightTheme(AppColors.black).theme,
           );
         },
       ),
